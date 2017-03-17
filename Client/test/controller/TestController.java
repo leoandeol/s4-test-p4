b6b4e341bd2674a4fdbd3cb795f268f3661cae0c
Leo Andeol <leo.andeol@gmail.com>
2017-03-17 16:19:59 +0100

    shitty controller tests

--- Client/test/controller/TestController.java
+++ Client/test/controller/TestController.java
@@ -1,14 +1,25 @@
 package controller;
 
+import model.Messagerie;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import view.Fenetre;
 
 public class TestController {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+    Exception ex;
+
+    @Test
+    public void test() {
+        try {
+            Messagerie m = new Messagerie();
+            Fenetre f = new Fenetre(m);
+            Controller.generateController(m, f);
+        } catch (Exception e) {
+            ex = e;
+        }
+        assertEquals(null, ex);
+    }
 
 }
